84e7683e2a502491fb25a310ef8f9a39df2f9802
Tarik Ünal <2108287@talnet.nl>
2020-10-24 16:44:39 +0200

    Taak 1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,17 @@
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`,`adres`,`postcode`,`geboortedatum`,`woonplaats`)
+VALUES ('5', 'Jan', 'Peters', '9A', 'SQLStraat 4', '1013TL', '2002-11-07', 'Amsterdam')
 
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`,`adres`,`postcode`,`geboortedatum`,`woonplaats`)
+VALUES ('6', 'Hans', 'Klok', '9A', 'Klokkenlaan 20', '1067PL', '1969-02-22', 'Amsterdam')
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`,`adres`,`postcode`,`geboortedatum`,`woonplaats`)
+VALUES ('7', 'Pieter', 'Post', '9A', 'Vanillestraat 57', '1026JK', '1974-08-26', 'Amsterdam')
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`,`adres`,`postcode`,`geboortedatum`,`woonplaats`)
+VALUES ('8', 'Jens', 'Timmerman', '9A', 'Hoekenes 64', '1069LT', '2000-08-14', 'Amsterdam')
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`,`adres`,`postcode`,`geboortedatum`,`woonplaats`)
+VALUES ('9', 'Tim', 'Boot', '9A', 'Lorentlaan 27', '1104MP', '2002-03-25', 'Amsterdam')
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`,`adres`,`postcode`,`geboortedatum`,`woonplaats`)
+VALUES ('10', 'Jonas', 'Sluiper', '9A', 'Korrelstraat 105', '1016NM', '2003-12-17', 'Amsterdam')
