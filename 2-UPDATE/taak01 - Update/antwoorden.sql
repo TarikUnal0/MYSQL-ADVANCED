266d648ca270138c5d07af7d49d3f4dc9c6f8205
Tarik Ünal <2108287@talnet.nl>
2020-10-26 19:39:02 +0100

    opdr 2

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,25 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET woonplaats = 'Amstelveen' 
 -- Opdracht 2 
+UPDATE studenten2
+SET adres = 'burgemeesterstraat 12', postcode = '1298LC'
+WHERE student_id = 8;
 
+UPDATE studenten2
+SET adres = 'Polerlaan 14', postcode = '1010AC'
+WHERE student_id = 9;
+
+UPDATE studenten2
+SET adres = 'Roterstraat 12', postcode = '1010AC'
+WHERE student_id = 10;
 -- Opdracht 3
-
+UPDATE studenten2 SET geboortedatum = '2000-12-12' WHERE student_id = 2
 -- Opdracht 4
+UPDATE studenten2 SET klas = '9C' WHERE student_id = 4
 
+UPDATE studenten2 SET klas = '9A' WHERE student_id = 8
 -- Opdracht 5
+UPDATE studenten2 
+SET voornaam = 'Johannah' 
+WHERE student_id = 6
